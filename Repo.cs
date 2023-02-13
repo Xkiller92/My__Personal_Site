4706bfadb6d1a7640ef0c42f49634b3199434f23
wissem gharbi <55883112+Xkiller92@users.noreply.github.com>
2023-02-13 13:37:52 +0100

    fixed the project image position to true centre

--- Repo.cs
+++ Repo.cs
@@ -1,4 +1,7 @@
 public class Repo{
     public string full_name { get; set; }
     public string description { get; set; }
+    public string GetRepoLink(){
+        return "https://github.com/" + full_name;
+    }
 }
